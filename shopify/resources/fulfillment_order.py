12d6cd19bbb3edc09f5d0dad68f6b3683f6281ac
Paulina Khew <paulina.khew@shopify.com>
2021-03-04 16:54:35 -0500

    Add support for finding fulfillmentorder using order_id

--- shopify/resources/fulfillment_order.py
+++ shopify/resources/fulfillment_order.py
@@ -2,4 +2,12 @@ from ..base import ShopifyResource
 
 
 class FulfillmentOrder(ShopifyResource):
-    pass
+    _prefix_source = "orders/$order_id/"
+
+    @classmethod
+    def _prefix(cls, options={}):
+        order_id = options.get("order_id")
+        if order_id:
+            return "%s/orders/%s" % (cls.site, order_id)
+        else:
+            return cls.site
--- test/fulfillment_order_test.py
+++ test/fulfillment_order_test.py
@@ -9,3 +9,10 @@ class FulfillmentOrderTest(TestCase):
         fulfillment_order = shopify.FulfillmentOrder.find(1046000778)
         self.assertEqual(690933842, fulfillment_order.shop_id)
         self.assertEqual("open", fulfillment_order.status)
+
+    def test_get_fulfillment_order_with_order_id(self):
+        self.fake("orders/450789469/fulfillment_orders", method="GET", body=self.load_fixture("fulfillment_orders"))
+        fulfillment_orders = shopify.FulfillmentOrder.find(order_id=450789469)
+        self.assertIsInstance(fulfillment_orders[0], shopify.FulfillmentOrder)
+        self.assertEqual(450789469, fulfillment_orders[0].order_id)
+        self.assertEqual(1046000777, fulfillment_orders[0].id)
