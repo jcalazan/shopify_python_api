f85a5023a8b68235d7c99ef3b5bea0723a234b4f
Nabeel Ahsen <nabeel.ahsen@shopify.com>
2021-03-10 11:25:33 -0500

    Support python 2.7

--- shopify/utils.py
+++ shopify/utils.py
@@ -1,56 +1,61 @@
 import jwt
 import re
-import urllib
+import sys
+if sys.version_info[0] < 3: # Backwards compatibility for python < v3.0.0
+    from urlparse import urljoin
+else:
+    from urllib.parse import urljoin
 
 
 class SessionTokenUtility():
     ALGORITHM = "HS256"
     PREFIX = 'Bearer '
     REQUIRED_FIELDS = ['iss', 'dest', 'sub', 'jti', 'sid']
-  
+
     @classmethod
-    def get_decoded_session_token(self, authorization_header, api_key, secret):
-        session_token = self.__extract_session_token(authorization_header)
-        decoded_payload = self.__decode_session_token(session_token, api_key, secret)
-        self.__validate_issuer(decoded_payload)
-  
+    def get_decoded_session_token(cls, authorization_header, api_key, secret):
+        session_token = cls.__extract_session_token(authorization_header)
+        decoded_payload = cls.__decode_session_token(session_token, api_key, secret)
+        cls.__validate_issuer(decoded_payload)
+
         return decoded_payload
   
     @classmethod
-    def __extract_session_token(self, authorization_header):
-        if not authorization_header.startswith(self.PREFIX):
+    def __extract_session_token(cls, authorization_header):
+        if not authorization_header.startswith(cls.PREFIX):
             raise ValueError() # TODO: Throw a more meaningful error
-  
-        return authorization_header[len(self.PREFIX):]
-  
+
+        return authorization_header[len(cls.PREFIX):]
+
     @classmethod
-    def __decode_session_token(self, session_token, api_key, secret):
+    def __decode_session_token(cls, session_token, api_key, secret):
         return jwt.decode(
             session_token,
             secret,
             audience=api_key,
-            algorithms=[self.ALGORITHM],
-            options={'require': self.REQUIRED_FIELDS}
+            algorithms=[cls.ALGORITHM],
+            options={'require': cls.REQUIRED_FIELDS}
         )
-  
+
     @classmethod
-    def __validate_issuer(self, decoded_payload):
-        self.__validate_issuer_hostname(decoded_payload)
-        self.__validate_issuer_and_dest_match(decoded_payload)
+    def __validate_issuer(cls, decoded_payload):
+        cls.__validate_issuer_hostname(decoded_payload)
+        cls.__validate_issuer_and_dest_match(decoded_payload)
+
+    @classmethod
+    def __validate_issuer_hostname(cls, decoded_payload):
+        hostname_pattern = r"[a-z0-9][a-z0-9-]*[a-z0-9]"
+        shop_domain_re = re.compile(r"^https://{h}\.myshopify\.com/$".format(h=hostname_pattern))
   
-    def __validate_issuer_hostname(decoded_payload):
-        hostname_pattern = r'[a-z0-9][a-z0-9-]*[a-z0-9]'
-        shop_domain_re = re.compile(fr'^https://{hostname_pattern}\.myshopify\.com/$')
-  
-        issuer_root = urllib.parse.urljoin(decoded_payload['iss'], '/')
-  
+        issuer_root = urljoin(decoded_payload['iss'], '/')
+
         if not shop_domain_re.match(issuer_root):
             raise ValueError() # TODO: Throw a more meaningful error
-  
-    def __validate_issuer_and_dest_match(decoded_payload):
-        issuer_root = urllib.parse.urljoin(decoded_payload['iss'], '/')
-        dest_root = urllib.parse.urljoin(decoded_payload['dest'], '/')
-  
+
+    @classmethod
+    def __validate_issuer_and_dest_match(cls, decoded_payload):
+        issuer_root = urljoin(decoded_payload['iss'], '/')
+        dest_root = urljoin(decoded_payload['dest'], '/')
+
         if issuer_root != dest_root:
             raise ValueError() # TODO: Throw a more meaningful error
-  
